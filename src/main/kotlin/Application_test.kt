8660ff92c1182c2e1c291d9a1216e6db00af8d06
Application_Belchenko <testylya333@gmail.com>
2025-09-06 11:39:24 +0300

    Init

--- src/main/kotlin/Application_test.kt
+++ src/main/kotlin/Application_test.kt
@@ -1,19 +1,82 @@
 fun main() {
 
-    println("Hello World!");
+    println("Hello World!")
 
-    var Value1 = 1;
-    println(Value1);
+    println("Choose the application (1-6)")
+    val input = readLine()
+    val value = input?.toIntOrNull()
 
-    Value1 = 5 * 2;
-    println(Value1);
+    when (value){
+        1 -> {
+            println("Enter any string")
 
-    Value1 = Value1/3;
-    println(Value1);
+            val input = readLine()!!
+            println(task1(input))
+        }
+        2 -> {
+            println("Enter any string")
 
-    println();
+            val input = readln()
+            println(task2(input))
+        }
+        3 -> {
+            println("Enter any integer")
 
-    var arr = arrayOf("1", 2, false, "Any");
-    arr.forEach { print(it.toString() + "\t") }
-    println("\n");
+            val input = readln()
+            println(task3(input))
+
+        }
+
+        else -> {
+            println("Wrong exit")
+        }
+
+    }
+
 }
+
+fun task1(input: String): String {
+    if (input.isEmpty()) return "the stack is empty"
+
+    var currentChar = input[0]
+    var count = 1
+    val result = StringBuilder()
+
+    for (i in 1 until input.length) {
+        if (input[i] == currentChar) {
+            count++
+        } else {
+            result.append(currentChar)
+            if (count > 1) result.append(count)
+            currentChar = input[i]
+            count = 1
+        }
+    }
+
+    result.append(currentChar)
+    if (count > 1) result.append(count)
+
+    return result.toString()
+}
+fun task2(input: String) {
+    val charCount = mutableMapOf<Char, Int>()
+
+    for (char in input) {
+        charCount[char] = charCount.getOrDefault(char, 0) + 1
+    }
+
+    val sortedKeys = charCount.keys.sorted()
+
+    for (key in sortedKeys) {
+        println("$key - ${charCount[key]}")
+    }
+}
+
+fun task3(input: ) {
+
+
+}
+
+
+
+
